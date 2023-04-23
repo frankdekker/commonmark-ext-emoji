f9fb536e89dec5adb045b71daab1c78458b063b0
Frank Dekker <fdekker@gmail.com>
2023-04-23 15:09:39 +0200

    Add resources

--- resources/test.php
+++ resources/test.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 // convert json to array
 
-$data = json_decode(file_get_contents('./full.json'), true, 512, JSON_THROW_ON_ERROR);
+$data = json_decode(file_get_contents('./shortcuts.json'), true, 512, JSON_THROW_ON_ERROR);
 
 $result = var_export($data, true);
-file_put_contents('./full.php', $result);
+file_put_contents('./shortcuts.php', $result);
 
